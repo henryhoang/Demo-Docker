e0bbe86c9a4bf3d21945a3f74af8681ad7a06637
henryhoang <henry.hoang@j2.com>
2014-10-27 16:17:33 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#cowsay
+#!/bin/sh
+
+clear
+
+echo '__________________'
+echo  "$*"
+echo "Hello world!"
+
+echo '------------------'
+
+
+
+echo '   \    ^__^'
+echo '    \   (oo) \_________'
+echo '        (__)\          )\/\'
+echo  '            | |------ W |'
+echo  '            | |       | |'
